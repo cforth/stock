166fccdc24df8089b80adb695bacda13f961837a
cforth <cforth@163.com>
2013-10-16 16:00:14 +0800

    增加了行业分类

--- script/hqTable.js
+++ script/hqTable.js
@@ -4,12 +4,12 @@ function putsDate() {
 }
 
 
-function putsStockQuotes(name, hqStr, myPrice, myDate) {
+function putsStockQuotes(name, hqStr, industry, myPrice, myDate) {
 
   var nowChange = ((hqStr[3] - hqStr[2]) / hqStr[2]) * 100;
   nowChange = nowChange.toFixed(2);
 
-  document.write("<tr><td>" + name +"</td><td>" + hqStr[0] + "</td><td>" + nowChange + "%</td><td>" + hqStr[3] + "</td><td>" + hqStr[2] + "</td><td>" + myPrice +"</td><td>" + myDate + "</td></tr>");
+  document.write("<tr><td>" + name +"</td><td>" + hqStr[0] + "</td><td>" + nowChange + "%</td><td>" + hqStr[3] + "</td><td>" + hqStr[2] + "</td><td>" + industry + "</td><td>" + myPrice +"</td><td>" + myDate + "</td></tr>");
 
 }
 
