9bf663caf8de2f3cc5dabbc7d71ce33d3596c194
cforth <cforth@163.com>
2013-10-17 09:42:27 +0800

    行情显示脚本代码优化

--- script/hqTable.js
+++ script/hqTable.js
@@ -7,9 +7,16 @@ function putsDate() {
 function putsStockQuotes(name, hqStr, industry, myPrice, myDate) {
 
   var nowChange = ((hqStr[3] - hqStr[2]) / hqStr[2]) * 100;
-  nowChange = nowChange.toFixed(2);
+  nowChange = nowChange.toFixed(2); 
 
-  document.write("<tr><td>" + name +"</td><td>" + hqStr[0] + "</td><td>" + nowChange + "%</td><td>" + hqStr[3] + "</td><td>" + hqStr[2] + "</td><td>" + industry + "</td><td>" + myPrice +"</td><td>" + myDate + "</td></tr>");
+  document.write("<tr><td>" + name +"</td>");     //股票代码
+  document.write("<td>" + hqStr[0] + "</td>");    //股票名称
+  document.write("<td>" + nowChange + "%</td>");  //当日涨跌
+  document.write("<td>" + hqStr[3] + "</td>");    //当前价格
+  document.write("<td>" + hqStr[2] + "</td>");    //昨日收盘价
+  document.write("<td>" + industry + "</td>");    //行业分类
+  document.write("<td>" + myPrice +"</td>");      //关注日期
+  document.write("<td>" + myDate + "</td></tr>"); //目标价格
 
 }
 
