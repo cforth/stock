adbe907dbc95378f2041980ee4310b50da71f5c7
cforth <cforth@163.com>
2013-10-25 09:39:48 +0800

    20131025增加股票,增加序号

--- js/hqTable.js
+++ js/hqTable.js
@@ -7,7 +7,7 @@ function putsIndexQuotes(hqStr) {
   document.write(hqStr[0] + ":" + hqStr[1] + "&nbsp" + hqStr[2] + "&nbsp" + hqStr[3] + "%");
 }
 
-function putsStockQuotes(name, hqStr, industry, myDate, bidPrice, forecastPrice) {
+function putsStockQuotes(id, name, hqStr, industry, myDate, bidPrice, forecastPrice) {
 
   var nowChange = ((hqStr[3] - hqStr[2]) / hqStr[2]) * 100;
   nowChange = nowChange.toFixed(2); 
@@ -15,15 +15,16 @@ function putsStockQuotes(name, hqStr, industry, myDate, bidPrice, forecastPrice)
   var totalChange = ((hqStr[3] - bidPrice) / bidPrice) * 100;
   totalChange = totalChange.toFixed(2);
 
-  document.write("<tr><td>" + name +"</td>");     //股票代码
-  document.write("<td>" + hqStr[0] + "</td>");    //股票名称
-  document.write("<td>" + nowChange + "%</td>");  //当日涨跌
-  document.write("<td>" + hqStr[3] + "</td>");    //当前价格
-  document.write("<td>" + hqStr[2] + "</td>");    //昨日收盘价
-  document.write("<td>" + industry + "</td>");    //行业分类
-  document.write("<td>" + myDate + "</td>"); //关注日期
-  document.write("<td>" + totalChange +"%</td>");  //累计涨跌
-  document.write("<td>" + forecastPrice +"</td></tr>");//目标价格
+  document.write("<tr><td>" + id +"</td>");             //序号
+  document.write("<td>" + name +"</td>");               //股票代码
+  document.write("<td>" + hqStr[0] + "</td>");          //股票名称
+  document.write("<td>" + nowChange + "%</td>");        //当日涨跌
+  document.write("<td>" + hqStr[3] + "</td>");          //当前价格
+  document.write("<td>" + hqStr[2] + "</td>");          //昨日收盘价
+  document.write("<td>" + industry + "</td>");          //行业分类
+  document.write("<td>" + myDate + "</td>");            //关注日期
+  document.write("<td>" + totalChange +"%</td>");       //累计涨跌
+  document.write("<td>" + forecastPrice +"</td></tr>"); //目标价格
 
 }
 
