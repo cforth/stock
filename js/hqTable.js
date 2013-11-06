f886a6b6c4c74b79f568e5592dd7a6379c59c957
cforth <cforth@163.com>
2013-11-06 14:18:42 +0800

    bugfix

--- js/hqTable.js
+++ js/hqTable.js
@@ -31,17 +31,17 @@ function putsStockQuotes(id, name, hqStr, industry, myDate, bidPrice, forecastPr
 
   //当股票当日停盘或者初始化而造成当前价格为0时，对涨跌幅不显示
   var nowChange = ((hqStr[3] - hqStr[2]) / hqStr[2]) * 100;
-  nowChange = (nowChange == -100) ? "- . --" : nowChange.toFixed(2); 
+  nowChange = (nowChange == -100) ? "---" : nowChange.toFixed(2); 
 
   var totalChange = ((hqStr[3] - bidPrice) / bidPrice) * 100;
-  totalChange = (totalChange == -100) ? "- . --" : totalChange.toFixed(2);
+  totalChange = (totalChange == -100) ? "---" : totalChange.toFixed(2);
 
   var nowPrice = parseFloat(hqStr[3]);
   nowPrice = nowPrice.toFixed(2);
 
   //溢价空间计算
   var premiumSpace = ((forecastPrice - hqStr[3]) / hqStr[3]) * 100;
-  premiumSpace = premiumSpace.toFixed(2);
+  premiumSpace = (premiumSpace == -100) ? "---" : premiumSpace.toFixed(2);
 
   //根据当日涨跌幅来显示当日涨跌列的颜色
   var colorNow = putsColor(nowChange);
